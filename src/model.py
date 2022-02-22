722efd74cdca77e9c89fdf2218c3e3b1ab20308f
ZeyuJing <92499213+ZeyuJing@users.noreply.github.com>
2022-02-21 21:39:07 -0800

    added missing 'lamb' to function 'sigmoid'

--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def sigmoid(x, lamb):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-lamb*x))
 
 class Model:
     def __init__(self, params):
